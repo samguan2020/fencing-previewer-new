956dd65b4221bf05868b088ed4e00e15e5b87a66
Kuizuo <hi@kuizuo.cn>
2023-07-17 19:59:53 +0800

    wip: 上传页面

---
+++ app/(main)/upload/Upload.tsx
@@ -0,0 +1,46 @@
+import React, { useCallback, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
+import { Button, Typography, Box } from '@mui/material';
+
+interface UploadProps {
+  onFileUpload: (file: File) => void;
+}
+
+const Upload: React.FC<UploadProps> = ({ onFileUpload }) => {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    setSelectedFile(acceptedFiles[0]);
+    onFileUpload(acceptedFiles[0]);
+  }, [onFileUpload]);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <Box display="flex" flexDirection="column" alignItems="center">
+      <Box {...getRootProps()} mb={2}
+        sx={{
+          width: "100%",
+          height: 300,
+          border: '1px dashed grey'
+
+        }}>
+        <div className='h-full flex flex-col justify-center items-center'>
+          <input {...getInputProps()} />
+          <Button variant="contained" component="span" >
+            {isDragActive ? '放下文件' : '选择文件'}
+          </Button>
+          <p className='mt-2'>Or Drag It Here</p>
+        </div>
+
+      </Box>
+      {selectedFile && (
+        <Typography variant="body1">
+          已选择文件: {selectedFile.name}
+        </Typography>
+      )}
+    </Box>
+  );
+};
+
+export default Upload;
--- app/(main)/upload/page.tsx
+++ app/(main)/upload/page.tsx
@@ -1,9 +1,42 @@
+'use client'
 
+import { Button, OutlinedInput, Stack } from "@mui/material";
+import Upload from './Upload'
+import { UploadFile as UploadFileIcon } from "@mui/icons-material";
 
 export default function UploadPage() {
+
+  const handleFileUpload = (file: File) => {
+    // 处理文件上传的逻辑
+    console.log('Selected file:', file);
+  };
+
   return (
-    <div>
-      test Enter
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-4 lg:p-8">
+
+      <div className="w-full">
+        <h2 className="font-semibold text-xl" >Upload Video</h2>
+
+        <div className="mt-2">
+          <p className="text-gray-500">Add YouTube URL Directly Here</p>
+          <Stack direction="row" spacing={2}>
+            <OutlinedInput
+              size="small"
+              fullWidth
+            />
+            <Button variant="contained">Add</Button>
+          </Stack>
+        </div>
+
+        <h3 className="mt-4 font-semibold">OR</h3>
+        <div className="mt-2">
+          <p className="text-gray-500">Upload Your Videos Here</p>
+          <Upload onFileUpload={handleFileUpload}></Upload>
+        </div>
+        <Button variant="contained" fullWidth startIcon={<UploadFileIcon />}>
+          Upload Video
+        </Button>
+      </div>
+    </main>
   );
 }
